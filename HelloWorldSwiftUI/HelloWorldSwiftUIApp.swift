71f4f81b31c90757975697ae47effa0d66e2c35c
Varya Utkina <varya2412@mail.ru>
2024-11-29 12:05:23 +0300

    Initial Commit

---
+++ HelloWorldSwiftUI/HelloWorldSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldSwiftUIApp.swift
+//  HelloWorldSwiftUI
+//
+//  Created by Варвара Уткина on 29.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloWorldSwiftUITests/HelloWorldSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldSwiftUITests.swift
+//  HelloWorldSwiftUITests
+//
+//  Created by Варвара Уткина on 29.11.2024.
+//
+
+import Testing
+@testable import HelloWorldSwiftUI
+
+struct HelloWorldSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
